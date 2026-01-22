f1e8e7bc8e066f26245356340e16c15dcc3ed72f
arch <ahorn103@gmx.de>
2026-01-22 20:10:17 +0100

    Fix Tailwind v4 config, change banner to blue

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <div class="fixed top-0 left-0 right-0 bg-green-500 text-white text-center py-2 font-bold z-50">
+    <div class="fixed top-0 left-0 right-0 bg-blue-500 text-white text-center py-2 font-bold z-50">
       Deployed via GitOps Pipeline - Tailwind 4
     </div>
     <NuxtWelcome />
